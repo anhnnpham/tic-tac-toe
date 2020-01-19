789d6e41ee2a3b78e3e57f1d10ba3ff9b18af3ad
Anh Pham <anhnnpham@gmail.com>
2020-01-19 23:23:32 +0200

    current

---
+++ main.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifndef MAIN_HPP
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <stdio.h>
+#include <cstring>
+#include <string>
+
+using namespace std;
+
+#endif // MAIN_HPP
